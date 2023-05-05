86d2278a5bdef5b5ef0ccfdb3b0a613790abfc3b
Dmitry Zebit <dimajebit@gmail.com>
2023-05-05 13:56:12 +0300

    Task6OnlinerTestsBySelenide
    Task6OnlinerTestsBySelenide

--- src/test/java/CatalogPage.java
+++ src/test/java/CatalogPage.java
@@ -7,12 +7,27 @@ import static com.codeborne.selenide.Selenide.$x;
 public class CatalogPage {
     private final ElementsCollection catalogSections =
             $$x("//li[@class='catalog-navigation-classifier__item ']");
+    private final ElementsCollection titlesOfComponents =
+            $$x("//div[text()=\" Комплектующие \"]/following-sibling::*//a/span/span[@class='catalog-navigation-list__dropdown-title']");
+    private final ElementsCollection descriptionOfComponents =
+            $$x("//div[text()=\" Комплектующие \"]/following-sibling::*//a/span/span[@class='catalog-navigation-list__dropdown-description']");
+    private final ElementsCollection computerComponents =
+            $$x("//div[text()=\" Комплектующие \"]/following-sibling::*//a");
     private final SelenideElement computersAndNetworksSection = $x("//li[@data-id='2']");
     private final SelenideElement verticalListOfComputersAndNetworks =
             $x("//div[@data-id='2']//div[@class='catalog-navigation-list__aside-list']");
-
+    private final SelenideElement computerComponentSection = $x("//div[text()=\" Комплектующие \"]");
     public ElementsCollection getCatalogSections() {
         return catalogSections;
+    }
+    public ElementsCollection getComputerComponents(){
+        return computerComponents;
+    }
+    public ElementsCollection getTitlesOfComponents(){
+        return titlesOfComponents;
+    }
+    public ElementsCollection getDescriptionOfComponents(){
+        return descriptionOfComponents;
     }
 
     public SelenideElement getVerticalListOfComputersAndNetworks() {
@@ -21,6 +36,10 @@ public class CatalogPage {
 
     public CatalogPage clickOnCatalogSectionComputersAndNetworksSection() {
         computersAndNetworksSection.click();
+        return new CatalogPage();
+    }
+    public CatalogPage clickOnComputerComponentSection(){
+        computerComponentSection.click();
         return new CatalogPage();
     }
 
--- src/test/java/OnlinerTest.java
+++ src/test/java/OnlinerTest.java
@@ -3,6 +3,8 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class OnlinerTest extends BaseTest {
 
     private final static String BASE_URL = "https://www.onliner.by/";
@@ -24,5 +26,28 @@ public class OnlinerTest extends BaseTest {
                 .getVerticalListOfComputersAndNetworks()
                 .shouldBe(Condition.visible)
                 .isDisplayed());
+    }
+    @Test
+    public void testComputerComponentsHaveDescriptionAndPrice(){
+        CatalogPage catalogPageWithComputersAndComponents = new MainPage(BASE_URL)
+                .clickOnCatalog()
+                .clickOnCatalogSectionComputersAndNetworksSection()
+                .clickOnComputerComponentSection();
+
+        int descriptionOfComponentsSize = catalogPageWithComputersAndComponents
+                .getDescriptionOfComponents()
+                .size();
+        System.out.println(descriptionOfComponentsSize);
+
+        int titlesOfComponentsSize = catalogPageWithComputersAndComponents
+                .getTitlesOfComponents()
+                .size();
+        System.out.println(titlesOfComponentsSize);
+
+        int computerComponentsSize = catalogPageWithComputersAndComponents.getComputerComponents().size();
+        System.out.println(computerComponentsSize);
+
+        Assert.assertTrue(computerComponentsSize==descriptionOfComponentsSize);
+        Assert.assertTrue(computerComponentsSize==titlesOfComponentsSize);
     }
 }
