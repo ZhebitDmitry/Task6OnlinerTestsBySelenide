4c90fd32ce6943e9a2902870ba4fe93f896782dc
Dmitry Zebit <dimajebit@gmail.com>
2023-05-13 15:50:29 +0300

    OnlinerTests
    OnlinerTests

---
+++ src/test/java/com/it_academy/test/BaseTest.java
@@ -0,0 +1,29 @@
+package com.it_academy.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+abstract public class BaseTest {
+
+    public static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true));
+        Configuration.browser = "chrome";
+        Configuration.driverManagerEnabled = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.headless = false;
+
+    }
+
+    @BeforeClass
+    public static void init() {
+        setUp();
+    }
+}
